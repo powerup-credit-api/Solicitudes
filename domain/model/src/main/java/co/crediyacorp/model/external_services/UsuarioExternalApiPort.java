346de34440b7f0aa4ef9d2d2753bef19858c1f34
Cristian Hernandez <cristianmanuel2304@hotmail.com>
2025-09-06 20:51:14 -0500

    dokcerizar microservicio

--- domain/model/src/main/java/co/crediyacorp/model/external_services/UsuarioExternalApiPort.java
+++ domain/model/src/main/java/co/crediyacorp/model/external_services/UsuarioExternalApiPort.java
@@ -5,7 +5,7 @@ import reactor.core.publisher.Mono;
 import java.math.BigDecimal;
 import java.util.List;
 
-public interface ExternalApiPort {
+public interface UsuarioExternalApiPort {
     Mono<Boolean> validarUsuario(String email, String documentoIdentidad);
 
     Mono<List<BigDecimal>> consultarSalarios(List<String> emails);
